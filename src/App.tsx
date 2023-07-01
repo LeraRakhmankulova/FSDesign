9e0a94f8e9d4c278f75b1c692992135b10d9c340
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-07-02 00:04:18 +0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 
 import {Link, Routes,} from "react-router-dom";
 import {Route} from "react-router";
@@ -6,18 +6,31 @@ import {Route} from "react-router";
 import {MainPageAsync} from "./pages/MainPage/MainPage.lazy";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.lazy";
 
+
+export enum Theme {
+    DEFAULT = 'default',
+    DARK = 'dark'
+}
+
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.DEFAULT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DEFAULT ? Theme.DARK : Theme.DEFAULT)
+    }
+
     return (
-        <>
+        <div className={`app ${theme}`}>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
+            <button onClick={toggleTheme}>Toggle</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path='/' element={<MainPageAsync/>}/>
                     <Route path='/about' element={<AboutPageAsync/>}/>
                 </Routes>
             </Suspense>
-        </>
+        </div>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
 
-import "./assets/styles/global.scss"
+import "./styles/index.scss"
 
 render(
     <BrowserRouter>
